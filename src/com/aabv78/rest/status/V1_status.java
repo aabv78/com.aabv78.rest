674ce4987bd27ce4c508474e105efb0efd9602cd
Armando Beleno <42255299+aabv78@users.noreply.github.com>
2018-11-02 12:17:27 -0400

    restful01
    Example of creating restful webservices with java and netbeans

---
+++ src/com/aabv78/rest/status/V1_status.java
@@ -0,0 +1,14 @@
+package com.aabv78.rest.status;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_status {
+
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+		return "<p>Java Web Service <p>";
+	}
+}
